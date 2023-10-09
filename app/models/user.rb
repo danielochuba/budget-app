a5fc9b02dbf46449e0056ebbaf1431e1c45f83fa
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-09 17:39:32 +0100

    establish user/deals relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :deals, foreign_key: 'author_id', dependent: :destroy
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
