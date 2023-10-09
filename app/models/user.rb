6918bd41e39c337db3a5ebf291bbce34046fb606
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-09 16:34:41 +0100

    add and install letter opener for email confirmations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_action :authenticate_user!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 end
