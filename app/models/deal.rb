d85551808215f925f148908cf620d69b3d7051ba
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-09 17:38:51 +0100

    add deals components and validation

---
+++ app/models/deal.rb
@@ -0,0 +1,6 @@
+class Deal < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  validates :name, presence: true , length: { maximum: 50 }
+  validates :amount, presence: true ,  numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
