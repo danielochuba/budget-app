5e0a864eb70fb58fb5385ee10d90a91d6f8cfc78
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-09 16:53:35 +0100

    add users controller and modify its model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  before_action :authenticate_user!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
