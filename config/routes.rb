6a215c30776968712cce654db7a7ae7e43b7b23e
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-11 16:41:51 +0100

    create nested route to handle deals

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :deals
+  resources :categories do
+    resources :deals
+  end
   devise_for :users
 
   root "home#index"
-
-  get "categories/:id/deals", to: "categories#deals", as: "category_deals"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
