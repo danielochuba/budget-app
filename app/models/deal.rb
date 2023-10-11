f3e8151ee9e2fdebcb3046b3ff4e65e482c37cab
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-11 11:11:33 +0100

    modify category and deal relationships

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -4,5 +4,5 @@ class Deal < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_and_belongs_to_many :categories, through: :categorydeal
+  has_and_belongs_to_many :categories, through: :categories_deals
 end
