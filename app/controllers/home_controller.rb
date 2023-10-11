1cc6c5e821c6e6922a2ae3e4e7f29f0ccd50478c
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-11 04:05:02 +0100

    fix linter error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
-
   def index
     if user_signed_in?
       redirect_to categories_path
     else
-      puts "Not signed in - redirecting to root_url"
     end
   end
 end
