cfa9d41dd1ca45b932542cad9e64c7b7864eb67d
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-11 04:01:33 +0100

    mdify root route to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :categories
   resources :deals
   devise_for :users
+
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
