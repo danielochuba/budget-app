114aebf92efc3ab372deb8495cef56be73727b5d
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-11 11:15:28 +0100

    modify join table

--- db/migrate/20231009205655_create_deal_category_table.rb
+++ db/migrate/20231009205655_create_deal_category_table.rb
@@ -1,6 +1,6 @@
 class CreateDealCategoryTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :categorydeal do |t|
+    create_table :categories_deals, id: false do |t|
       t.references :deal, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
