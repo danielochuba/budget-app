1000b8ab40058cf5c969c2edd7a8171a7c7b13d6
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-14 04:07:02 +0100

    fix failing test

---
+++ spec/views/category/index.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Please Customize the email and password with yours: categories/index', type: :feature do
+  describe 'index page' do
+    let(:user) { User.create(id: 1, name: 'Daniel Ochuba', email: 'danielochuba78@gmail.com', password: 'Welcome126@') }
+
+    let(:category) do
+      Category.create(id: 1, name: 'Sample Category', icon: 'Sample Icon',
+                      user_id: user.id)
+    end
+    before do
+      visit root_path
+    end
+
+    it 'renders the index view' do
+      expect(page).to have_content('Budget')
+    end
+  end
+end
